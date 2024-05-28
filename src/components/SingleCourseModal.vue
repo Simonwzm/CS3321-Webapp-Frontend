e5c121f3f3d10a47dab1161c7ee0b76442d9c525
Simon <simon_wzm@163.com>
2024-05-28 14:04:58 +0800

    [main]: bugfix on course modal visualization and searchPage list visualization

--- src/components/SingleCourseModal.vue
+++ src/components/SingleCourseModal.vue
@@ -1,46 +1,39 @@
 
 <template>
     <div class="course-contain flex w-5/6 h-full justify-start bg-white bg-opacity-80 p-20 " style="border-radius: 20px; max-height: 1226px; overflow:hidden">
-        <div class="course-data-container w-1/2 h-full mr-4  max-h-full" style="overflow:scroll">
-            <n-card  embedded :bordered="true" style="position:relative; overflow: scroll">
+        <div class="course-data-container w-1/2 h-full mr-4  max-h-full" >
+            <n-card  embedded :bordered="true" style="position:relative; overflow: scroll"
+            >
                         <h1 style="font-size:25px; font-weight: bold">{{ course_info.title }}</h1>
-                            <div class="priv-item-container flex-col justify-start align-middle max-h-16 overflow-clip">
+                            <div class="priv-item-container flex-col justify-start align-middle">
                                 <!-- <div v-for="(priv_item, index) in this.limitedPrivData(course_info.priv_data)" :key="index"> -->
-                                <div v-for="(priv_item, index) in course_info.priv_data" :key="index">
-                                    <h3 style="font-weight: bold">
-                                        {{ priv_item }}
+                                <div v-for="(priv_item, index) of Object.entries(course_info.priv_data)" :key="index">
+                                    <h3 style="font-weight: bold" v-if="priv_item[1] !== null && priv_item[0] != 'extern_obj'">
+                                        {{ priv_item[0] }}
                                     </h3>
+                                    <span v-if="priv_item[0]!=='link' && priv_item[1] !== null && priv_item[0] != 'extern_obj'">
+                                      {{ priv_item[1] }}
+                                    </span>
+                                    <ul v-if="priv_item[0]=='link'" v-for="priv_item_link in priv_item[1]">
+                                      <li class="mb-2">
+                                        - {{ priv_item_link }}
+                                      </li>
+                                    </ul>
                                 </div>
                             </div>
 
-                            <template  #footer>
+                            <!-- <template  #footer>
                             <h3>Footer</h3>
-                            </template>
+                            </template> -->
                             <template  #action >
                             <n-space>
-                                <n-button size="small">
+                                <n-button size="small" v-for="(link, index) in this.links" @click="handleLinkClick(link)">
                                 <template #icon>
                                     <n-icon>
                                     <cash-icon />
                                     </n-icon>
                                 </template>
-                                1$
-                                </n-button>
-                                <n-button size="small">
-                                <template #icon>
-                                    <n-icon>
-                                    <cash-icon />
-                                    </n-icon>
-                                </template>
-                                10$
-                                </n-button>
-                                <n-button size="small">
-                                <template #icon>
-                                    <n-icon>
-                                    <cash-icon />
-                                    </n-icon>
-                                </template>
-                                100$
+                                link{{ index+1 }}
                                 </n-button>
                             </n-space>
                             </template>
@@ -59,7 +52,7 @@
                 </div>
         </div>
 
-        <div class="coure-graph-container w-1/2 h-full ml-4">
+        <div class="coure-graph-container w-1/2 h-full ml-4" style="overflow:visible">
             <GraphComponent :init_search="this.computed_course_name"> </GraphComponent>
         </div>
 
@@ -73,6 +66,8 @@ import SingleFileModal from './SingleFileModal.vue'
 
 import { CashOutline as CashIcon } from "@vicons/ionicons5";
 import { IosAirplane } from '@vicons/ionicons4'
+import { useMessage } from 'naive-ui'
+
 
 export default {
   props: {
@@ -85,7 +80,8 @@ export default {
     SingleFileModal,
     GraphComponent,
     CashIcon,
-    IosAirplane
+    IosAirplane,
+    useMessage
   },
   data() {
     return {
@@ -97,14 +93,40 @@ export default {
     limitedPrivData(ob) {
       return Object.entries(ob).slice(0, this.limit);
     },
+    handleLinkClick(link) {
+      // copy link to user clipboard
+      navigator.clipboard.writeText(link);
+      window.alert("Link copied to clipboard")
+    },
   },
   computed: {
     computed_course_name() {
         if (this.course_info.course_name == "大学物理（荣誉）（1）") {
-            return "CS1604"
+            return "UNIVERSITY_PHYSICS_1"
+        }
+        if (this.course_info.course_name == "大学物理（荣誉）（2）") {
+            return "UNIVERSITY_PHYSICS_2"
+        }
+        if (this.course_info.course_name == "操作系统") {
+          return "OS"
+        }
+        if (this.course_info.course_name == "程序设计原理与方法") {
+          return "CS1604"
         }
       return "CS1605"
-    }
+    },
+    local_props() {
+      console.log(this.course_info.priv_data)
+      return this.course_info
+    },
+    links() {
+      for (let [key, value] of Object.entries(this.course_info.priv_data)) {
+        if (key == "link") {
+          return value
+        }
+      }
+      return null
+    },
   }
 }
 </script>
